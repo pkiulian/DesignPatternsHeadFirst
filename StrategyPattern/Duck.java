bad4568d1ddbbf0c040783e44b1124df2dc4874d
Iulian Peca <pkiulian@gmail.com>
2014-01-29 20:27:24 +0100

    Duck fresh

---
+++ StrategyPattern/Duck.java
@@ -0,0 +1,94 @@
+public class Duck{
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public Duck(){}
+
+    public abstract void display();
+
+    public void performFly(){
+	flyBehavior.fly();
+    }
+
+    public void performQuack(){
+	quackBehavior.quack();
+    }
+
+    public void swim(){
+	System.out.println("all ducks float, even decoys!");
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
